bd0fd7ff18fbce1bb91f1f325568eda639a84839
peakflys <peakflys@gmail.com>
2013-12-01 00:34:53 -0800

    update virtual function test

--- virtualTest.cpp
+++ virtualTest.cpp
@@ -1,3 +1,8 @@
+/**
+ *\brief virtual function test case
+ *\author peakflys
+ *\date Sun Dec  1 14:32:47 CST 2013
+ */
 #include <iostream>
 
 using namespace std;
@@ -5,20 +10,34 @@ using namespace std;
 class Base
 {
 public:
-	//param is not dynamic
-	virtual void print(const int a = 10) {cout<<a<<endl;}
+	virtual void print(const int a = 10) {cout<<"Base: "<<a<<endl;}
 };
 
 class Derive : public Base
 {
 public:
-	virtual void print(const int a = 100) {cout<<a<<endl;}
+	virtual void print(const int a = 100) {cout<<"Derive: "<<a<<endl;}
 };
 
 int main()
 {
 	Base *pb = new Derive;
 	pb->print();
+	Base& rb = *pb;
+	rb.print();
+	Derive d;
+	d.print();
+	Base *pbb = &d;
+	pbb->print();
+	Base& rbb = d;
+	rbb.print();
+	Base b;
+	b.print();
+	Derive *pd = (Derive*)&b;
+	pd->print();
+	Derive& rd = *(Derive*)&b;
+	rd.print();
+	delete pb;
 	return 0;
 }
 
